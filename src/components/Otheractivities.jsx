3e106b4a9b7f2c770c7262cf1d58e7aad8f836b2
Swapnil Kulkarni <swapnilkulkarni987@gmail.com>
2023-02-11 10:46:17 +0530

    message "in development" added

--- src/components/Otheractivities.jsx
+++ src/components/Otheractivities.jsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const Otheractivities = () => {
     return (
-        <div className="flex flex-col">
-            
+        <div className="flex flex-col h-[30rem]">
+            <div className="flex justify-center items-center h-full">
+                <h1 className="text-xl font-poppins font-bold">This Section is in development</h1>
+            </div>
         </div>
     );
 }
